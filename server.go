9751ac1739906e163629d9770a84193de354eaff
nasrul0807 <nasrulmuhammad748@gmail.com>
2022-04-12 15:52:27 +0700

    feat: add endpoint event

--- server.go
+++ server.go
@@ -6,8 +6,10 @@ import (
 	"tupulung/deliveries/routes"
 	"tupulung/utilities"
 
+	eventRepository "tupulung/repositories/event"
 	userRepository "tupulung/repositories/user"
 	authService "tupulung/services/auth"
+	eventService "tupulung/services/event"
 	userService "tupulung/services/user"
 
 	"github.com/labstack/echo/v4"
@@ -20,11 +22,17 @@ func main() {
 
 	e := echo.New()
 
-	// User 
+	// User
 	userRepository := userRepository.NewUserRepository(db)
 	userService := userService.NewUserService(userRepository)
 	userHandler := handlers.NewUserHandler(userService)
 	routes.RegisterUserRoute(e, userHandler)
+
+	// Event
+	eventRepository := eventRepository.NewEventRepository(db)
+	eventService := eventService.NewEventService(eventRepository, userRepository)
+	eventHandler := handlers.NewEventHandler(eventService)
+	routes.RegisterEventRoute(e, eventHandler)
 
 	// Authentication
 	authService := authService.NewAuthService(userRepository)
