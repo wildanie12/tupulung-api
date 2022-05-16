05a2d262fbf7194938a290956262f81d2c532bcd
nasrul0807 <nasrulmuhammad748@gmail.com>
2022-05-16 23:21:16 +0700

    adjust: moving jwt to middleware and add read token

--- deliveries/middleware/jwt.go
+++ deliveries/middleware/jwt.go
@@ -1,6 +1,9 @@
 package middleware
 
 import (
+	"time"
+	"tupulung/entities"
+
 	"github.com/golang-jwt/jwt"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
@@ -8,7 +11,26 @@ import (
 
 func JWTMiddleware() echo.MiddlewareFunc {
 	return middleware.JWTWithConfig(middleware.JWTConfig{
-		SigningKey: []byte("jeweteuwu"),
+		SigningKey:    []byte("jeweteuwu"),
 		SigningMethod: jwt.SigningMethodHS256.Name,
 	})
+}
+
+func CreateToken(user entities.User) (string, error) {
+	claim := jwt.MapClaims{
+		"name":   user.Name,
+		"email":  user.Email,
+		"userID": user.ID,
+		"exp":    time.Now().Add(time.Hour * 6).Unix(),
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claim)
+	return token.SignedString([]byte("jeweteuwu"))
+}
+
+func ReadToken(token interface{}) (int, error) {
+	tokenID := token.(*jwt.Token)
+	claims := tokenID.Claims.(jwt.MapClaims)
+	id := int(claims["userID"].(float64))
+	return id, nil
 }
